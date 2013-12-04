44d90847ccb766f20d7a739c8cacbc0818195798
Baron Chu <baron.chu@hotmail.com>
2013-12-04 17:08:59 +0800

    test reset hard

--- src/Hello.java
+++ src/Hello.java
@@ -2,5 +2,8 @@
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		System.out.println("Hello World1!");
+		System.out.println("Hello World2!");
+		System.out.println("Hello World3!");
 	}
 }
